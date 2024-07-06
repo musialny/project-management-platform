8b6cf97014b4f0e39cd3ff2e7659d60c4d196b9a
Łukasz Musiał <lukasz.musial@musialny.dev>
2024-07-06 14:21:17 +0200

    Setup cli-preprocessor tests

--- vcs/shared/cli-preprocessor/tests/CliCommandExecutorTests.cpp
+++ vcs/shared/cli-preprocessor/tests/CliCommandExecutorTests.cpp
@@ -1,9 +1,8 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
+import vcs.shared.cli_preprocessor.CliCommandExecutor;
+
 TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
-    // Expect equality.
     EXPECT_EQ(7 * 6, 42);
 }
